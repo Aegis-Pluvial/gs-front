fb62bf77586bd17480916fc38c8bfed2a7d6c05d
luccas <luccasfigueiragc@gmail.com>
2025-06-05 06:23:09 -0300

    carosel ajustado e implementado

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,108 @@ function toggleNav() {
     navMenu.style.display = 'none'; // Oculta o menu
   }
 }
+let currentIndex = 0;
+const carousel = document.getElementById('carousel');
+const totalImages = carousel.children.length;
+
+function updateCarousel() {
+  carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+  updateCarousel();
+}
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalImages;
+  updateCarousel();
+}
+
+
+let toqueinicial = 0;
+let toquefinal = 0;
+
+carousel.addEventListener('touchstart', (e) => {
+  toqueinicial = e.changedTouches[0].screenX;
+});
+
+carousel.addEventListener('touchend', (e) => {
+  toquefinal = e.changedTouches[0].screenX;
+  handleGesture();
+});
+
+function handleGesture() {
+  const swipeDistance = toquefinal - toqueinicial;
+  const swipeThreshold = 50; 
+
+  if (swipeDistance > swipeThreshold) {
+  
+    prevSlide();
+  } else if (swipeDistance < -swipeThreshold) {
+  
+    nextSlide();
+  }
+  
+}
+const slidesData = [
+  {
+    title: "alguma coisa",
+    content: "lorem ipsum"
+  },
+  {
+    title: "alguma coisa",
+    content: "lorem ipsum"
+  },
+  {
+    title: "alguma coisa",
+    content: "lorem ipsum"
+  }
+];
+
+
+
+function updateCarousel() {
+  carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+  updateText();
+}
+
+function updateText() {
+  const textTitle = document.getElementById('text-title');
+  const textContent = document.getElementById('text-content');
+  const dynamicText = document.getElementById('dynamic-text');
+  
+  if (!textTitle || !textContent || !dynamicText) return;
+  
+  
+  dynamicText.classList.add('text-fade-out');
+  
+  
+  setTimeout(() => {
+    textTitle.textContent = slidesData[currentIndex].title;
+    textContent.textContent = slidesData[currentIndex].content;
+    
+    
+    dynamicText.classList.remove('text-fade-out');
+    dynamicText.classList.add('text-fade-in');
+    
+    setTimeout(() => {
+      dynamicText.classList.remove('text-fade-in');
+    }, 300);
+  }, 150);
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+  updateCarousel();
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalImages;
+  updateCarousel();
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  updateText();
+});
+
